311e59f7afb386050258dd2b1a15425425f1c64e
Duhan Mo <moduhan@Duhans-MacBook-Air.local>
2024-11-13 19:01:40 +0900

    feat: implement calculator

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,28 @@
+package calculator
+
+class Calculator {
+    fun add(
+        a: Double,
+        b: Double,
+    ): Double = a + b
+
+    fun subtract(
+        a: Double,
+        b: Double,
+    ): Double = a - b
+
+    fun multiply(
+        a: Double,
+        b: Double,
+    ): Double = a * b
+
+    fun divide(
+        a: Double,
+        b: Double,
+    ): Double {
+        if (b == 0.0) {
+            throw IllegalArgumentException("Division by zero is not allowed")
+        }
+        return a / b
+    }
+}
---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,34 @@
+package calculator
+
+import io.kotest.assertions.throwables.shouldThrowWithMessage
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : StringSpec({
+    "덧셈을 하면 두 수를 더한다" {
+        val sut = Calculator()
+        sut.add(1.0, 2.0) shouldBe 3
+    }
+
+    "뺄셈을 하면 두 수를 뺀다" {
+        val sut = Calculator()
+        sut.subtract(1.0, 2.0) shouldBe -1
+    }
+
+    "곱하기를 하면 두 수를 곱한다" {
+        val sut = Calculator()
+        sut.multiply(3.0, 2.0) shouldBe 6
+    }
+
+    "나누기를 하면 두 수를 나눈다" {
+        val sut = Calculator()
+        sut.divide(10.0, 2.0) shouldBe 5
+    }
+
+    "어떤 수를 0으로 나누면 예외 발생한다" {
+        val sut = Calculator()
+        shouldThrowWithMessage<IllegalArgumentException>("Division by zero is not allowed") {
+            sut.divide(10.0, 0.0)
+        }
+    }
+})
