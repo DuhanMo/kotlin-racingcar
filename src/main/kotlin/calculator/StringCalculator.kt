ceb14684753883a55b2d7dfee3e6a1b0bd382d97
Duhan Mo <moduhan@Duhans-MacBook-Air.local>
2024-11-13 22:52:48 +0900

    style: apply formatting changes

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -7,11 +7,11 @@ class StringCalculator {
         val operators = parseResult.operators
         val calculator = Calculator()
 
-        val result =
-            numbers.drop(1).zip(operators)
-                .fold(numbers[0]) { acc, (number, operator) ->
-                    calculator.calculate(acc, number, operator)
-                }
-        return if (result % 1.0 == 0.0) result.toInt().toString() else result.toString()
+        return numbers.drop(1).zip(operators)
+            .fold(numbers[0]) { acc, (number, operator) ->
+                calculator.calculate(acc, number, operator)
+            }.let { result ->
+                if (result % 1.0 == 0.0) result.toInt().toString() else result.toString()
+            }
     }
 }
