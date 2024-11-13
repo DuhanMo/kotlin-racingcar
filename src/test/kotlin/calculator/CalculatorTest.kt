026b1e6d7508e48a86e35becce3284514199fe55
Duhan Mo <moduhan@Duhans-MacBook-Air.local>
2024-11-13 19:12:49 +0900

    test: use shouldBeExactly for precise type comparison

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -2,27 +2,27 @@ package calculator
 
 import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.doubles.shouldBeExactly
 
 class CalculatorTest : StringSpec({
     "덧셈을 하면 두 수를 더한다" {
         val sut = Calculator()
-        sut.add(1.0, 2.0) shouldBe 3
+        sut.add(1.0, 2.0) shouldBeExactly 3.0
     }
 
     "뺄셈을 하면 두 수를 뺀다" {
         val sut = Calculator()
-        sut.subtract(1.0, 2.0) shouldBe -1
+        sut.subtract(1.0, 2.0) shouldBeExactly -1.0
     }
 
     "곱하기를 하면 두 수를 곱한다" {
         val sut = Calculator()
-        sut.multiply(3.0, 2.0) shouldBe 6
+        sut.multiply(3.0, 2.0) shouldBeExactly 6.0
     }
 
     "나누기를 하면 두 수를 나눈다" {
         val sut = Calculator()
-        sut.divide(10.0, 2.0) shouldBe 5
+        sut.divide(10.0, 2.0) shouldBeExactly 5.0
     }
 
     "어떤 수를 0으로 나누면 예외 발생한다" {
